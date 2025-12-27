cd5e511f7a43efaeab9d21b11d915e4c9400943e
ljr <ljr>
2025-12-27 21:35:55 +0800

    langchain文件

--- Langchain/RestaurantNameGenerator/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-if cuisine:
-#     response = langchain_helper.generate_restaurant_name_and_items(cuisine)
-#     st.header(response['restaurant_name'].strip())
-#     menu_items = response['menu_items'].strip().split(",")
-#     st.write("**Menu Items**")
-#     for item in menu_items:
-#         st.write("-", item)
